de9847653616ecfa349ffe75691a45324d7f57ac
richardfreiree <richardzzfreirezz@gmail.com>
2025-09-11 22:18:30 -0300

    commit

--- app/src/main/java/br/com/etecia/myapp/RecuperarSenhaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/RecuperarSenhaActivity.java
@@ -1,10 +1,6 @@
 package br.com.etecia.myapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
-ImageView backMenuMain;
-TextView forgotPassword;
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.menu_principal_layout);
+        setContentView(R.layout.recuperar_senha_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        backMenuMain=findViewById(R.id.backMenuMain);
-        forgotPassword=findViewById(R.id.forgotPassword);
-
-        backMenuMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-            }
         });
     }
 }
